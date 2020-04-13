3bc99fd5cfc336a0f7e30a994d0453c2c2c69957
yosadade <masyosad@gmail.com>
2020-04-13 20:44:13 +0700

    component content

--- src/components/content/index.jsx
+++ src/components/content/index.jsx
@@ -344,18 +344,206 @@ class Content extends Component {
                   </button>
                 </form>
                 <div className='single-sidebar-widget user-info-widget'>
-                  <img className='img' src={require('../../assets/img/IMG_20190218_141330.jpg')} />
+                  <img className='img' src={require('../../assets/img/user-info.png')} />
                   <a>
-                    <h4>Yosada Dede</h4>
+                    <h4 className='name'>Yosada Dede</h4>
                   </a>
-                  <p style={{marginTop: 0, marginBottom: '1rem' }}>
+                  <p className='job'>
                     Frontend Developer
                   </p>
+                  <ul className='social-links justify-content-start row mx-3'>
+                    <li className='li-social-links'>
+                      <a>
+                        <img className='icon-sosmed' src={require('../../assets/icon/facebook-brands.svg')} />
+                      </a>
+                    </li>
+                    &nbsp;
+                    <li className='li-social-links'>
+                      <a>
+                        <img className='icon-sosmed' src={require('../../assets/icon/instagram-brands.svg')} />
+                      </a>
+                    </li>
+                    &nbsp;
+                    <li className='li-social-links'>
+                      <a>
+                        <img className='icon-sosmed' src={require('../../assets/icon/github-brands.svg')} />
+                      </a>
+                    </li>
+                    &nbsp;
+                    <li className='li-social-links'>
+                      <a>
+                        <img className='icon-sosmed' src={require('../../assets/icon/linkedin-in-brands.svg')} />
+                      </a>
+                    </li>
+                  </ul>
+                  <p>Boot camps have its supporters andit sdetractors. Some people do not understand why you should have to spend money on boot camp when you can get. Boot camps have itssuppor ters andits detractors.</p>
                 </div>
-                <div className='single-sidebar-widget popular-post-widget' />
-                <div className='single-sidebar-widget ads-widget' />
-                <div className='single-sidebar-widget post-category-widget' />
-                <div className='single-sidebar-widget newsletter-widget' />
+                <div className='single-sidebar-widget popular-post-widget'>
+                  <a className='btn-primary btn-popular-post text-center btn-block text-light'>Popular Posts</a>
+                  <div className='popular-post-list mt-4'>
+                    <div className='single-post-list d-flex flex-row'>
+                      <div className='thumb'>
+                        <img className='img-fluid-popular-post' src={require('../../assets/img/pp1.jpg')} />
+                      </div>
+                      <div className='details'>
+                        <a>
+                          <h6 style={{ fontWeight: 'bold', color: 'black' }}>Space The Final Frontier</h6>
+                        </a>
+                        <p>02 Hours ago</p>
+                      </div>
+                    </div>
+                    <div className='single-post-list d-flex flex-row'>
+                      <div className='thumb'>
+                        <img className='img-fluid-popular-post' src={require('../../assets/img/pp2.jpg')} />
+                      </div>
+                      <div className='details'>
+                        <a>
+                          <h6 style={{ fontWeight: 'bold', color: 'black' }}>Space The Final Frontier</h6>
+                        </a>
+                        <p>02 Hours ago</p>
+                      </div>
+                    </div>
+                    <div className='single-post-list d-flex flex-row'>
+                      <div className='thumb'>
+                        <img className='img-fluid-popular-post' src={require('../../assets/img/pp3.jpg')} />
+                      </div>
+                      <div className='details'>
+                        <a>
+                          <h6 style={{ fontWeight: 'bold', color: 'black' }}>Space The Final Frontier</h6>
+                        </a>
+                        <p>02 Hours ago</p>
+                      </div>
+                    </div>
+                    <div className='single-post-list d-flex flex-row'>
+                      <div className='thumb'>
+                        <img className='img-fluid-popular-post' src={require('../../assets/img/pp4.jpg')} />
+                      </div>
+                      <div className='details'>
+                        <a>
+                          <h6 style={{ fontWeight: 'bold', color: 'black' }}>Space The Final Frontier</h6>
+                        </a>
+                        <p>02 Hours ago</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='single-sidebar-widget ads-widget'>
+                  <a href='#'>
+                    <img className='img-fluid' src={require('../../assets/img/ads-banner.jpg')} alt='' />
+                  </a>
+                </div>
+                <div className='single-sidebar-widget post-category-widget'>
+                  <a className='btn-primary btn-popular-post text-center btn-block text-light'>Post Categories</a>
+                  <ul className='cat-list'>
+                    <li>
+                      <a className='d-flex justify-content-between'>
+                        <p>Technology</p>
+                        <p>37</p>
+                      </a>
+                    </li>
+                    <li>
+                      <a className='d-flex justify-content-between'>
+                        <p>Lifestyle</p>
+                        <p>24</p>
+                      </a>
+                    </li>
+                    <li>
+                      <a className='d-flex justify-content-between'>
+                        <p>Fashion</p>
+                        <p>58</p>
+                      </a>
+                    </li>
+                    <li>
+                      <a className='d-flex justify-content-between'>
+                        <p>Art</p>
+                        <p>29</p>
+                      </a>
+                    </li>
+                    <li>
+                      <a className='d-flex justify-content-between'>
+                        <p>Food</p>
+                        <p>29</p>
+                      </a>
+                    </li>
+                    <li>
+                      <a className='d-flex justify-content-between'>
+                        <p>Architecture</p>
+                        <p>09</p>
+                      </a>
+                    </li>
+                    <li>
+                      <a className='d-flex justify-content-between'>
+                        <p>Adventure</p>
+                        <p>44</p>
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+                <div className='single-sidebar-widget newsletter-widget'>
+                  <a className='btn-primary btn-popular-post text-center btn-block text-light'>News Letter</a>
+                  <p>Here, I focus on a range of items and features that we use in life without giving them a second thought. </p>
+                  <div className='form-group d-flex flex-row'>
+                    <input className='form-control' style={{ width: '70%', borderRadius: '0px', fontSize: '14px' }} placeholder='Enter Email' type='text' />
+                    <button className='px-4' style={{ width: '30%' }}>
+                      <i className='fa fa-envelope' />
+                    </button>
+                  </div>
+                  <p className='text-center mt-3'>You Can unsubscribe at any time</p>
+                </div>
+                <div className='single-sidebar-widget tag-cloud-widget'>
+                  <a className='btn-primary btn-popular-post text-center btn-block text-light'>Tag Cloud</a>
+                  <ul className='row li-tags my-3 ml-1'>
+                    <li>
+                      <a>Technology</a>
+                    </li>
+                    &nbsp;
+                    <li>
+                      <a>Fashion</a>
+                    </li>
+                    &nbsp;
+                    <li>
+                      <a>Architecture</a>
+                    </li>
+                    &nbsp;
+                    <li>
+                      <a>Fashion</a>
+                    </li>
+                    &nbsp;
+                    <li>
+                      <a>Food</a>
+                    </li>
+                    &nbsp;
+                    <li>
+                      <a>Technology</a>
+                    </li>
+                    &nbsp;
+                    <li>
+                      <a>Lifestyle</a>
+                    </li>
+                    &nbsp;
+                    <li>
+                      <a>Art</a>
+                    </li>
+                    &nbsp;
+                    <li>
+                      <a>Adventure</a>
+                    </li>
+                    &nbsp;
+                    <li>
+                      <a>Food</a>
+                    </li>
+                    &nbsp;
+                    <li>
+                      <a>Lifestyle</a>
+                    </li>
+                    &nbsp;
+                    <li>
+                      <a>Adventure</a>
+                    </li>
+                    &nbsp;
+                  </ul>
+
+                </div>
               </div>
             </div>
           </div>
